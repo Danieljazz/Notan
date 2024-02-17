0d6eed159788dc9e0d7c67e6b544fdecab0b9b21
Dan <66785111+Danieljazz@users.noreply.github.com>
2024-02-17 16:02:16 +0100

    New logo and continue working on Header

--- src/components/landing-page/header.tsx
+++ src/components/landing-page/header.tsx
@@ -8,7 +8,10 @@ import {
   NavigationMenuList,
   NavigationMenuTrigger,
 } from "@radix-ui/react-navigation-menu";
+import Link from "next/link";
 import React from "react";
+import Image from "next/image";
+import logo from "../../../public/NotanLogo.png";
 
 const Header = () => {
   const components: { title: string; href: string }[] = [
@@ -19,12 +22,19 @@ const Header = () => {
   ];
 
   return (
-    <header className="w-full flex flex-col">
+    <header className="flex justify-end items-center p-6">
+      <Link href="/" className="w-full">
+        <Image src={logo} height={80} width={80} alt="Logo"></Image>
+      </Link>
       <NavigationMenu>
         <NavigationMenuList className="hidden md:block">
           <NavigationMenuItem>
             <NavigationMenuTrigger>Features</NavigationMenuTrigger>
-            <NavigationMenuContent></NavigationMenuContent>
+            <NavigationMenuContent>
+              <ul className="grid gap-3 p-4 md:w-[400px] lg:w-[500px] lg:grid-cols-[.75fr_1fr]">
+                <ListItem title="Hi"></ListItem>
+              </ul>
+            </NavigationMenuContent>
           </NavigationMenuItem>
         </NavigationMenuList>
       </NavigationMenu>
@@ -41,8 +51,15 @@ const ListItem = React.forwardRef<
   return (
     <li>
       <NavigationMenuLink asChild>
-        <a ref={ref} className={cn()} {...props}>
-          <div>{title}</div>
+        <a
+          ref={ref}
+          className={cn(
+            "block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground",
+            className
+          )}
+          {...props}
+        >
+          <div className="text-sm font-medium leading-none">{title}</div>
           <p>{children}</p>
         </a>
       </NavigationMenuLink>
